5f912fbd86dfd3be938aaf08a1c2dc131d16f473
Narendra Kumar Vadapalli <narenandu@gmail.com>
2014-10-16 01:22:27 +0530

    latest version on 15th oct 2014, still needs some work

--- essentials.h
+++ essentials.h
@@ -1,5 +1,6 @@
-
-
+#include <limits.h>
+#include <stdlib.h>
+#include <math.h>
 
 /* 
  This would be my basic Vector Class for storing color values
@@ -16,11 +17,10 @@ class Vector3
         T z;
       
     public:
-        Vector3() : x(T(0)), y(T(0)), z(T(0)) {}
+        Vector3(T val) : x(val), y(val), z(val) {}
         Vector3(T x=0, T y=0,T z=0) : x(x), y(y), z(z) {}
         template <typename K>
         friend ostream& operator<< (ostream &out, const Vector3<K> &c);
-
 };
 
 
@@ -31,3 +31,43 @@ ostream& operator<< (ostream &out, const Vector3<T> &vec)
                      << vec.y << ','
                      << vec.z << ')';
 }
+
+
+/* 
+ Color Class which would be mainly used for 
+*/
+class Color
+{
+    public:
+        unsigned char r;
+        unsigned char g;
+        unsigned char b;
+
+    public:
+        Color(unsigned char r, unsigned char g, unsigned char b) : r(r), g(g), b(b) {}
+        Color* Vector3toColor(Vector3<unsigned char> inVector);
+        void operator=(const Color &c)
+        {
+            r = c.r;
+            g = c.g;
+            b = c.b;
+        }
+        friend ostream& operator<< (ostream &out, const Color &c);
+};
+
+ostream& operator<< (ostream &out, const Color &c)
+{
+    return out << c.r << c.g << c.b ;
+}
+
+Color Vector3toColor(Vector3<float> inVector)
+{
+    Color outColor(0,0,0);
+    // limiting value between 0 - 1 and then scaling it in 255 (UCHAR_MAX) colors 
+    outColor.r = std::min(1.0f, std::max(0.0f, inVector.x)) * UCHAR_MAX;
+    outColor.g = std::min(1.0f, std::max(0.0f, inVector.y)) * UCHAR_MAX;
+    outColor.b = std::min(1.0f, std::max(0.0f, inVector.z)) * UCHAR_MAX;
+
+    return outColor;
+}
+
--- rayTrace.cpp
+++ rayTrace.cpp
@@ -1,22 +1,50 @@
-#include <iostream>
+#include <iostream.h>
+#include <fstream.h>
 #include "essentials.h"
 
 // IMAGE SETTINGS (resolution)
-#define RES_X 320
-#define RES_Y 240
+#define RES_X 80 //image width
+#define RES_Y 60 //image height
+
+#define MAX_RAY_BOUNCES 3
 
 int main()
 {
-    //Vector3<unsigned int> c1(12,13,14);
-	Vector3<unsigned int> c1(0);
-    Vector3<float> f1(6.1,5.0,3.0);
-    Vector3<char> c2('a','b','c');
+	// initialize the image data to be written out
+	Vector3<float> *img;
+	img = new Vector3<float>[RES_X * RES_Y];
 
-    cout<<c1;
-    cout<<endl<<f1;
-    cout<<endl<<c2;
-    cout<<endl<<sqrt(4);
-    cout<<endl;
+	// Loop through each pixel from 0,0 to RES_X,RES_Y
+    for (int j = 0; j < RES_Y; j++)
+    {
+    	for (int i = 0; i < RES_X; i++)
+    	{
+
+    		//TO_DO: Write the actual tracing code which would calculate
+    		// the ray's starting point (from eye to middle of the pixel) and 
+    		// the direction in to the scene. Will be creating only a single sphere
+
+    		*img = Vector3<float> (255, 0, 255);
+    		img++;
+    	}
+    }
+
+    // Write out the image data to binary ppm format
+    // .ppm files can be viewed using gimp on linux
+    // or photoshop on windows
+    ofstream output("sphere_traced.ppm", ios::out | ios::binary);
+    output << "P6\n" << RES_X << "\n" << RES_Y << "\n" << UCHAR_MAX << "\n";
+
+    for (int i = 0; i < RES_X * RES_Y; i++)
+    {
+    	cout<<img[i];
+    	Color color = Vector3toColor(img[i]);
+    	output << color;
+    }
+
+    output.close();
+    delete [] img;
+    
     return 0;
 }
 
