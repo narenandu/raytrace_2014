1d3107196df326f64db67165f78745082e642c0c
Narendra Kumar Vadapalli <narenandu@gmail.com>
2014-10-15 23:11:11 +0530

    pushing intermediate version, still preliminary

--- essentials.h
+++ essentials.h
@@ -1,28 +1,33 @@
+
+
+
+/* 
+ This would be my basic Vector Class for storing color values
+ or vectors. For Vectors there will be operations like dot product,
+ magnitude calculation etc..
+*/
+
 template <typename T>
-class Color
+class Vector3
 {
     private:
-        T r;
-        T g;
-        T b;
+        T x;
+        T y;
+        T z;
       
     public:
-        Color(T red=255, T green=255,T blue=255)
-        {
-            r = red;
-            g = green;
-            b = blue;
-        }
-
+        Vector3() : x(T(0)), y(T(0)), z(T(0)) {}
+        Vector3(T x=0, T y=0,T z=0) : x(x), y(y), z(z) {}
         template <typename K>
-        friend ostream& operator<< (ostream &out, const Color<K> &c);
+        friend ostream& operator<< (ostream &out, const Vector3<K> &c);
 
 };
 
+
 template <typename T>
-ostream& operator<< (ostream &out, const Color<T> &c)
+ostream& operator<< (ostream &out, const Vector3<T> &vec)
 {
-    return out << '('<< c.r << ','
-                     << c.g << ','
-                     << c.b << ')';
+    return out << '('<< vec.x << ','
+                     << vec.y << ','
+                     << vec.z << ')';
 }
--- rayTrace.cpp
+++ rayTrace.cpp
@@ -1,16 +1,22 @@
-#include <iostream.h>
+#include <iostream>
 #include "essentials.h"
+
+// IMAGE SETTINGS (resolution)
+#define RES_X 320
+#define RES_Y 240
 
 int main()
 {
-    Color<unsigned int> c1(12,13,14);
-    Color<float> f1(6.1,5.0,3.0);
-    Color<char> c2('a','b','c');
+    //Vector3<unsigned int> c1(12,13,14);
+	Vector3<unsigned int> c1(0);
+    Vector3<float> f1(6.1,5.0,3.0);
+    Vector3<char> c2('a','b','c');
 
     cout<<c1;
     cout<<endl<<f1;
     cout<<endl<<c2;
-    cout<<"\nhello world.. I am back\n";
+    cout<<endl<<sqrt(4);
+    cout<<endl;
     return 0;
 }
 
